36df5fd423531fa275526403e03b24b8380c359f
Leonardo Arantes <leonardorarantes95@gmail.com>
2022-07-12 19:39:13 -0300

    test: fix deploy

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 import { TransactionTypes } from "../repositories/cardRepository.js";
+
 import * as cardService from "../services/cardService.js";
 
 export async function createCard(req: Request, res: Response) {
